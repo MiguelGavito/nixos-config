5fc8cec877bda482f32ca0077e1249200d9c32b7
MiguelGavito <mgavitogzz@gmail.com>
2025-12-15 17:00:08 -0600

    trying fix the gnome cursor

--- hosts/elnavio/home.nix
+++ hosts/elnavio/home.nix
@@ -4,6 +4,14 @@
   imports = [
     ../../homeManagerModules
   ];
+  
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      cursor-theme = "Adwaita";
+      cursor-size = 24;
+    };
+  };
+
 
   home.stateVersion = "25.11";
 }
