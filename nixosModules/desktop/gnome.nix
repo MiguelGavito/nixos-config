ace45bc5d7cee7721530d686ea924ca566f3e4f8
MiguelGavito <mgavitogzz@gmail.com>
2025-12-15 00:34:30 -0600

    little test of gnome and the configuration of the cursor and basic theme

--- nixosModules/desktop/gnome.nix
+++ nixosModules/desktop/gnome.nix
@@ -9,10 +9,27 @@
     services.desktopManager.gnome.enable = true;
   
     programs.hyprland.enable = false;
+    
+    # save gnome settings
+    programs.dconf.enable = true;
 
     # Portals para integración
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ ];
+    xdg.portal.extraPortals = with pkgs; [ 
+      xdg-desktop-portal-gnome
+    ];
+
+    # package needed for the cursor and theme
+    environment.systemPackages = with pkgs; [
+      gnome-themes-extra
+      adwaita-icon-theme
+    ];
+
+    # env variable for cursor
+    environment.variables = {
+      XCURSOR_THEME = "Adwaita";
+      XCURSOR_THEME = "24";
+    };
   };
 }
 
