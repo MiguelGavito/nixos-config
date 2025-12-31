0580ea9976d2e9927d456eb32add068559d2d368
MiguelGavito <mgavitogzz@gmail.com>
2025-12-30 23:37:07 -0600

    changed terminal shortcut on gnome, now it uses kitty as well, is still alt+shift+t but it can be changed later

--- hosts/elnavio/home.nix
+++ hosts/elnavio/home.nix
@@ -38,7 +38,7 @@
     
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       name = "Terminal";
-      command = "kgx";
+      command = "kitty";
       binding = "<Control><Alt>t";
     };
   };
