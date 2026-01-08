8a13c00779598ff05b5fba974efef7e5f85b910f
MiguelGavito <mgavitogzz@gmail.com>
2026-01-07 20:24:35 -0600

    created vars folder for common variables and configuration , will modified later that

---
+++ vars/default.nix
@@ -0,0 +1,14 @@
+{ lib }:
+{
+  username = "miguelagg";
+  userfullname = "Miguel Gavito";
+  useremail = "mgavitogzz@gmail.com";
+
+  mainSshAuthorizedKeys = [
+    # later add them
+  ];
+  secondaryAuthorizedKeys = [
+    # backup later
+
+  ];
+}
