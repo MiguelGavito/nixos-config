2350bbb6db9d948fb1f5ddea02a1c2696afe364c
MiguelGavito <mgavitogzz@gmail.com>
2026-01-29 23:00:38 -0600

    the output of the flake now autodetect the aarch and the nix flake check do not fail

--- outputs/default.nix
+++ outputs/default.nix
@@ -13,16 +13,16 @@
     noctalia = inputs.noctalia;
   };
 
+  system = builtins.currentSystem or "unknown";
+
   # Import system-specific outputs
   x86_64-linux = import ./x86_64-linux (args // {system = "x86_64-linux";});
   aarch64-darwin = import ./aarch64-darwin (args // {system = "aarch64-darwin";});
-in {
-  # Merge all nixosConfigurations (Linux)
-  nixosConfigurations = x86_64-linux.nixosConfigurations or {};
-
-  # Merge all darwinConfigurations (macOS)
-  darwinConfigurations = aarch64-darwin.darwinConfigurations or {};
-
+in 
+{
+  nixosConfigurations = if system == "x86_64-linux" then x86_64-linux.nixosConfigurations else {};
+  darwinConfigurations = if system == "aarch64-darwin" then aarch64-darwin.darwinConfigurations else {};
+      
   # Merge all homeConfigurations
   homeConfigurations = 
     (x86_64-linux.homeConfigurations or {}) //
